a0b9b360fa3e8210d01673b48023f3ec45ad7c35
Viktor Kolot <kolot87@yandex.ru>
2023-10-29 16:45:04 +0500

    Used pylint

--- flask_try.py
+++ flask_try.py
@@ -1,14 +1,31 @@
+""" Used libraries in the code """
 from flask import Flask
-
-
-
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def page_index():
-    return"Главная страничка"
+# @app.route("/")
+# def page_index():
+#     return"Главная страничка"
+
+
+
+# @app.route("/profile/")
+# def page_profile():
+#     return "Профиль пользователя"
+
+
+@app.route("/line/<deep>")
+def page_profile(deep):
+    """Function return user lent in the page"""
+    print(deep)
+    print(type(deep))
+    return f"<h1>Лента пользователя {deep}</h1>"
+
+
+
+
+
 
 
 app.run()
